51c8d2009cce189b4aa85a462e066dffedd7f00a
Arijit Banerjee <arijit.codes@gmail.com>
2022-12-08 14:21:15 +0530

    ✨ Setup Comfigs

---
+++ src/configs/config.sample.js
@@ -0,0 +1,7 @@
+const PORT = 5000;
+const MONGODB_CONNECTION_URI = "";
+
+module.exports = {
+  PORT,
+  MONGODB_CONNECTION_URI,
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const app = require("./app");
-
-const PORT = 5000;
+const { PORT } = require("./configs/config");
 
 try {
   app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
+    console.log(`✨ Server started at http://localhost:${PORT}`);
   });
 } catch (error) {
-  console.error("Failed to start server! ", error);
+  console.error("🔴 Failed to start server! ", error);
 }
