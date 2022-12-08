aa26b65da2110c6c429d7fcd027584c2956ae1ed
Arijit Banerjee <arijit.codes@gmail.com>
2022-12-08 14:09:07 +0530

    ✨ Removed Views

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.send("🦄🌈✨👋🏻🌎🌍🌏👋🏻✨🌈🦄");
 });
 
 module.exports = router;
